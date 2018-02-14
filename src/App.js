1d3c5517a47a4beec7aac1fd0f9666450442d624
Your Name <you@example.com>
2018-02-14 16:38:39 +1100

    delete the lock

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import { MainLayout, CmsLayout, ResultNotificationLayout } from './components/la
 
 // topPanel, headerContent, headerNav, leftSiderNav, mainContent, footer, notification,
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-              <Switch>
-                <Route path="/resultNotification" exact component={ResultNotificationLayout} />
-                <Route path="/CMS" exact component={CmsLayout} />
-                <Route path="/" component={MainLayout} />
-              </Switch>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/resultNotification" exact component={ResultNotificationLayout} />
+          <Route path="/CMS" exact component={CmsLayout} />
+          <Route path="/" component={MainLayout} />
+        </Switch>
+      </div>
+    );
+  }
 }
 export default App;
 
