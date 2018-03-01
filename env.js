afa53c2a10e7369efe66b4ebfb0c610c6e216c7b
Your Name <you@example.com>
2018-03-02 10:55:22 +1100

    delte evn.js

--- env.js
+++
@@ -1,9 +0,0 @@
-// 不同环境需要配置不同的URL
-module.exports = {
-  // API_URL  : "http://api.cloudhubpanellocal.com/v1",
-  //API_URL: 'http://api.breakabletest.com/v1', // 测试环境
-  API_URL : "https://api.staging.breakable.com/v1",//staging环境
-  // API_URL : "https://api.cloudhubpanel.com/v1"//线上环境
-
-};
-
