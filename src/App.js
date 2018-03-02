3167bd994c1cc25b82737264e96909640c30f3e9
Your Name <you@example.com>
2018-03-02 11:36:49 +1100

    fix the input style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import './assets/less/index.less';
 import { MainLayout, AuthLayout } from './components/layout/index';
 
 class App extends React.Component {
