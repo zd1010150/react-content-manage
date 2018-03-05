6eb615961a7bd04fb7953d0f000637ea60777bcb
Your Name <you@example.com>
2018-03-05 13:58:22 +1100

    add radio checkbox

--- src/views/ui-demo/component/buttons.jsx
+++ src/views/ui-demo/component/buttons.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-shadow */
 import React from 'react';
 import { Button, Radio, Icon } from 'antd';
-import { Panel } from 'components/ui/index';
 
 
 const Buttons = () => (
@@ -18,6 +17,15 @@ const Buttons = () => (
       <Button className="ml-sm account-theme-btn" >account</Button>
       <Button className="ml-sm opport-theme-btn" >opport</Button>
       <Button className="ml-sm report-theme-btn" >report</Button>
+      <h2> eillipse buttons</h2>
+      <Button className="btn-ellipse ml-sm lead-theme-btn" size="small">lead ellipse</Button>
+      <Button className="btn-ellipse ml-sm account-theme-btn" size="small">account ellipse</Button>
+      <Button className="btn-ellipse ml-sm opport-theme-btn" size="small">opport ellipse</Button>
+      <Button className="btn-ellipse ml-sm report-theme-btn" size="small">report ellipse</Button>
+      <h2> icon buttons</h2>
+      <Button shape="circle" icon="search" />
+      <Button className="ml-sm lead-theme-btn" shape="circle" icon="search" />
+
   </div>
 );
 
