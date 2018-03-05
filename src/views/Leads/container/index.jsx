c3a94d83e4070c83580c40a8ab79b2c975e14bd6
lcxdi8887 <lcxdi8887@gmail.com>
2018-03-05 14:41:21 +1100

    temp commit to sync with remote

--- src/views/Leads/container/index.jsx
+++ src/views/Leads/container/index.jsx
@@ -5,10 +5,6 @@ import { Row } from 'antd';
 import Topbar from './Topbar';
 
 class Leads extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
   render() {
     return (
