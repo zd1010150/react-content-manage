cfbdcb64b64790d766099252413e0db23c14c2f3
lcxdi8887 <lcxdi8887@gmail.com>
2018-03-06 17:55:37 +1100

    add fixed pagination

--- src/views/ui-demo/component/Pagination.jsx
+++ src/views/ui-demo/component/Pagination.jsx
@@ -3,7 +3,6 @@ import { Pagination } from 'antd';
 const PaginationExample = () => {
   return (
     <Fragment>
-      <br />
       <h4>Basic pagination component</h4>
       <p>The original antd styles have been globally overwritten</p>
       <Pagination
@@ -11,6 +10,17 @@ const PaginationExample = () => {
         showTotal={(total, range) => `showing ${range[0]}-${range[1]} of ${total} items`}
         pageSize={20}
         defaultCurrent={1}
+        size="small"
+      />
+      <br />
+      <h4>Fixed position pagination component</h4>
+      <p>The original antd styles have been globally overwritten with extra class 'stickToRight'</p>
+      <Pagination
+        total={850}
+        showTotal={(total, range) => `${range[0]}-${range[1]} / ${total}`}
+        pageSize={20}
+        defaultCurrent={1}
+        className="stickToRight"
         size="small"
       />
     </Fragment>
