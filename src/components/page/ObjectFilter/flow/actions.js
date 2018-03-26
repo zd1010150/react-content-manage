c3ff535f644f6c83bde7e3c32514787197d04bb2
lcxdi8887 <lcxdi8887@gmail.com>
2018-03-26 17:44:35 +1100

    fix no fields be set when create new

--- src/components/page/ObjectFilter/flow/actions.js
+++ src/components/page/ObjectFilter/flow/actions.js
@@ -24,33 +24,43 @@ export const fetchViewById = (
   id,
   objectType,
 ) => dispatch => get(getFetchUrlById(id, objectType), {}, dispatch).then(json => {
-  if (json
+  if (id !== Enums.PhantomID) {
+    if (json
       && !_.isEmpty(json.all_field)
       && !_.isEmpty(json.all_field.data)
       && !_.isEmpty(json.list_view)
       && !_.isEmpty(json.list_view.data)) {
 
-    const { all_field, list_view } = json;
+      const { all_field, list_view } = json;
 
-    const allFieldData = all_field.data;
-    const {
-      view_name,
-      condition_logic,
-      selectors,
-      assign_option,
-      assign_to_users,
-      assign_to_teams
-    } = list_view.data;
-    // set section 1
-    dispatch(setViewName(view_name));
-    // set section 2
-    dispatch(setConditionLogic(condition_logic));
-    // set section 3
-    dispatch(setAvailableFields(allFieldData));
-    dispatch(addToSelection(selectors.data));
-    // set section 4
-    dispatch(setVisibilityOption(assign_option));
-    dispatch(batchAddToSelection(assign_to_users, assign_to_teams));
-    
+      const allFieldData = all_field.data;
+      const {
+        view_name,
+        condition_logic,
+        selectors,
+        assign_option,
+        assign_to_users,
+        assign_to_teams
+      } = list_view.data;
+      // set section 1
+      dispatch(setViewName(view_name));
+      // set section 2
+      dispatch(setConditionLogic(condition_logic));
+      // set section 3
+      dispatch(setAvailableFields(allFieldData));
+      dispatch(addToSelection(selectors.data));
+      // set section 4
+      dispatch(setVisibilityOption(assign_option));
+      dispatch(batchAddToSelection(assign_to_users, assign_to_teams));
+    }
+  } else {
+    if (json
+      && !_.isEmpty(json.all_field)
+      && !_.isEmpty(json.all_field.data)) {
+
+      const { all_field } = json;
+      const allFieldData = all_field.data;
+      dispatch(setAvailableFields(allFieldData));
+    }
   }
 });
