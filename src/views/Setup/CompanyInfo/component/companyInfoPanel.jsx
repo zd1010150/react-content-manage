b970354db2c2c108b7f1c81f5c43beb7f4dc4fa6
Your Name <you@example.com>
2018-03-26 19:04:15 +1100

    analyse the field

--- src/views/Setup/CompanyInfo/component/companyInfoPanel.jsx
+++ src/views/Setup/CompanyInfo/component/companyInfoPanel.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-shadow */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, notification } from 'antd';
+import { Row, Col } from 'antd';
 import classNames from 'classnames/bind';
 import { intlShape, injectIntl } from 'react-intl';
 import { Panel, EditBox } from 'components/ui/index';
@@ -41,78 +41,155 @@ class companyInfoPanel extends React.Component {
           <Row className="info-display-table-row">
             <Col className="gutter-row" span={12}>
               <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.name' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}> <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.name} onBlur={value => this.onBlur('name', value)} /></Col>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.name' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}> <EditBox isShowStatuLabel={false} spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.name} onBlur={value => this.onBlur('name', value)} /></Col>
               </Row>
             </Col>
             <Col className="gutter-row" span={12}>
               <Row>
-                <Col className="gutter-row field-label" span={12}> { formatMessage({ id: 'page.comInfo.phone' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.phone} onBlur={value => this.onBlur('phone', value)} /></Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row className="info-display-table-row">
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.fax' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.fax} onBlur={value => this.onBlur('fax', value)} /></Col>
-              </Row>
-            </Col>
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.address' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.address} onBlur={value => this.onBlur('address', value)} /></Col>
+                <Col className="gutter-row field-label" span={12}> <span className="form-label">{ formatMessage({ id: 'page.comInfo.phone' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>  <EditBox isShowStatuLabel={false} spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="input" value={company.phone} onBlur={value => this.onBlur('phone', value)} /></Col>
               </Row>
             </Col>
           </Row>
           <Row className="info-display-table-row">
             <Col className="gutter-row" span={12}>
               <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.country' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="select" value={company.country_code} options={countriesOptions} onBlur={value => this.onBlur('country_code', value, true, countries)} /></Col>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.fax' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="input"
+                    value={company.fax}
+                    onBlur={value => this.onBlur('fax', value)}
+                  />
+                </Col>
               </Row>
             </Col>
             <Col className="gutter-row" span={12}>
               <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.fiscalYearStarts' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="select" options={yearsOptions} value={company.fiscal_year_starts} onBlur={value => this.onBlur('fiscal_year_starts', value)} /></Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row className="info-display-table-row">
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.timeZone' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}>  <EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="select" value={company.time_zone} options={timeZoneOptions} onBlur={value => this.onBlur('time_zone', value, true, timeZones)} /></Col>
-              </Row>
-            </Col>
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.usedDataSpace' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}> <span className={cx('edit-box-span')}>{ company.used_data_space } </span></Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row className="info-display-table-row">
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.fileStorage' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}><span className={cx('edit-box-span')}>{ company.file_storage } </span></Col>
-              </Row>
-            </Col>
-            <Col className="gutter-row" span={12}>
-              <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.lastModify' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}><span className={cx('edit-box-span')}>{company.user && company.user.name} {company.updated_at}</span></Col>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.address' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="input"
+                    value={company.address}
+                    onBlur={value => this.onBlur('address', value)}
+                  />
+                </Col>
               </Row>
             </Col>
           </Row>
           <Row className="info-display-table-row">
             <Col className="gutter-row" span={12}>
               <Row>
-                <Col className="gutter-row field-label" span={12}>{ formatMessage({ id: 'page.comInfo.language' }) }:</Col>
-                <Col className="gutter-row field-value" span={12}><EditBox spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="select" options={languageOptions} value={company.language} onBlur={value => this.onBlur('language', value, true, languages)} /></Col>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.country' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="select"
+                    value={company.country_code}
+                    options={countriesOptions}
+                    onBlur={value => this.onBlur('country_code', value, true, countries)}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.fiscalYearStarts' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="select"
+                    options={yearsOptions}
+                    value={company.fiscal_year_starts}
+                    onBlur={value => this.onBlur('fiscal_year_starts', value)}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="info-display-table-row">
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.timeZone' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="select"
+                    value={company.time_zone}
+                    options={timeZoneOptions}
+                    onBlur={value => this.onBlur('time_zone', value, true, timeZones)}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.usedDataSpace' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="input"
+                    isDisabled
+                    value={company.used_data_space}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="info-display-table-row">
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.fileStorage' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}><span className={cx('edit-box-span')}>{ company.file_storage } </span>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="input"
+                    isDisabled
+                    value={company.file_storage}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.lastModify' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox
+                    isShowStatuLabel={false}
+                    spanClasses={cx('edit-box-span')}
+                    inputClasses={cx('edit-box-input')}
+                    type="input"
+                    isDisabled
+                    value={`${company.user && company.user.name} ${company.updated_at}`}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="info-display-table-row">
+            <Col className="gutter-row" span={12}>
+              <Row>
+                <Col className="gutter-row field-label" span={12}><span className="form-label">{ formatMessage({ id: 'page.comInfo.language' }) }:</span></Col>
+                <Col className="gutter-row field-value" span={12}>
+                  <EditBox isShowStatuLabel={false} spanClasses={cx('edit-box-span')} inputClasses={cx('edit-box-input')} type="select" options={languageOptions} value={company.language} onBlur={value => this.onBlur('language', value, true, languages)} />
+                </Col>
               </Row>
             </Col>
           </Row>
