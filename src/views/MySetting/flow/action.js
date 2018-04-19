669e6ab56ae64e27cff2ea57cd2c7b036e817204
Your Name <you@example.com>
2018-04-19 17:14:44 +1000

    fix the avatar upload error

--- src/views/MySetting/flow/action.js
+++ src/views/MySetting/flow/action.js
@@ -2,9 +2,9 @@ import { patch } from 'store/http/httpAction';
 import _ from 'lodash';
 import { MY_SETTING_SET_AVATOR } from './actionType';
 
-export const setAvator = avator => ({
+export const setAvator = avatar => ({
   type: MY_SETTING_SET_AVATOR,
-  avator,
+    avatar,
 });
 
 export const updatePwd = (args, cb) => dispatch => patch('/admin/users/me/reset_password', { ...args }, dispatch).then(() => {
