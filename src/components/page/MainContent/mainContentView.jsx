e46c359e7d0da1ef4aad88d3b7fcfdee63c7cc97
Your Name <you@example.com>
2018-04-26 17:55:48 +1000

    fix the global search bug

--- src/components/page/MainContent/mainContentView.jsx
+++ src/components/page/MainContent/mainContentView.jsx
@@ -41,42 +41,30 @@ const MainContent = () => (
     <Route path="/setup/company-info/company-info" component={CompanyInfo} />
     <Route path="/setup/company-info/users" component={Users} />
     <Route path="/setup/company-info/chart" component={OrganisationChart} />
-    <Route path="/user/email-setting" component={EmailTemplates} />
-    <Route path="/setup/email/templates" component={EmailTemplates} />
-    <Route
-      path="/setup/email/template-edit/:templateId"
-      component={EmailTemplatesCreation}
-    />
-    <Route
-      path="/setup/email/templates-creation"
-      component={EmailTemplatesCreation}
-    />
-    <Route path="/email/new" component={NewEmail} />
-    <Route path="/setup/email/campaign" component={EmailCampaign} exact />
-    <Route
-      path="/setup/email/campaign/edit/:campaignId"
-      component={EditCampaign}
-    />
-    <Route path="/setup/email/campaign/new" component={NewCampaign} />
-    <Route
-      path="/setup/company-info/permissions"
-      component={PermissionProfile}
-    />
+    <Route path="/setup/company-info/permissions" component={PermissionProfile} />
     <Route path="/setup/:objectType/fields" component={Fields} />
     <Route path="/setup/:objectType/pageLayout" component={Layouts} />
-    <Route path="/my-setting" component={MySetting} />
+    <Route path="/setup/email/templates" component={EmailTemplates} />
+    <Route path="/setup/email/template-edit/:templateId" component={EmailTemplatesCreation} />
+    <Route path="/setup/email/templates-creation" component={EmailTemplatesCreation} />
+    <Route path="/setup/email/campaign" component={EmailCampaign} exact />
+    <Route path="/setup/email/campaign/edit/:campaignId" component={EditCampaign} />
+    <Route path="/setup/email/campaign/new" component={NewCampaign} />
 
+    <Route path="/my-setting" component={MySetting} />
+    <Route path="/email/new" component={NewEmail} />
+    <Route path="/user/email-setting" component={EmailTemplates} />
     <Route path="/dashboard" component={Dashboard} exact />
-    <Route path="/:objectType/sharing/:objectId" component={FindDuplicates} />
-    <Route path="/:objectType/find/:objectId" component={FindDuplicates} />
+    <Route path="/globalSearch" component={GlobalSearch} exat />
+
     <Route path="/leads/convert/find/:objectId" component={FindDuplicates} />
     <Route path="/leads/convert/convert/:objectId" component={ConvertLeads} />
     <Route path="/leads/merge/" component={MergeLeads} />
 
+    <Route path="/:objectType/sharing/:objectId" component={FindDuplicates} />
+    <Route path="/:objectType/find/:objectId" component={FindDuplicates} />
     <Route path="/:objectType/:objectId/attachments/0000-0000" component={ClientAttachments} />
     <Route path="/:objectType/:objectId/tasks/:taskId" component={ObjectTask} />
-    <Route path="/dashboard" component={Dashboard} exact />
-    <Route path="/globalSearch" component={GlobalSearch} exat />
     <Route path="/:objectType/views/:viewId" component={ObjectView} />
     <Route
       path="/:objectType/:objectId"
