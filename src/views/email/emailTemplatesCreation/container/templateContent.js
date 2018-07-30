d2950d7026f6c95a881c467eeb259473e22de0d9
Wayne Cen <waynecen@ITzerologixs-iMac-3.local>
2018-07-30 17:46:14 +1000

    bugfix-wayne-crm_issue#178 update

--- src/views/email/emailTemplatesCreation/container/templateContent.js
+++ src/views/email/emailTemplatesCreation/container/templateContent.js
@@ -108,7 +108,6 @@ class TemplateContent extends React.Component {
             // An error occurred during upload.
             data.message = response[1];
             evt.cancel();
-            console.log('something');
         } else {
             data.url = JSON.parse(response[0]).data.url
         }
