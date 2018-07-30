9f2791d88ada035f660591ff83c99abcc8dabcca
lcxdi8887 <lcxdi8887@gmail.com>
2018-07-30 17:47:37 +1000

    test push

--- src/components/page/FilterCriteria__REFACTORED/i18n.js
+++ src/components/page/FilterCriteria__REFACTORED/i18n.js
@@ -6,6 +6,10 @@ export default {
     valueTip: '使用逗号可以分隔多个筛选条件，但不适用于日期或时间选择器',
     condTip: '不同条件适用于不同类型的字段',
     emptyMsg: '请先从左侧选择任一字段',
+    errors: {
+      incorrectType: '筛选逻辑或筛选规则类型有误！',
+      displayNumMissing: '缺少相应的筛选规则编号！',
+    },
   },
   en: {
     field: 'Field',
@@ -14,5 +18,9 @@ export default {
     valueTip: 'Use comma to separate filter rules except for Date or Datetime field.',
     condTip: 'Different type of fields work with different conditions set.',
     emptyMsg: 'Please select a field firstly on the left-hand side.',
+    errors: {
+      incorrectType: 'Logic or Criteria are with incorrect type!',
+      displayNumMissing: 'Some display nums are absent from the logic!',
+    },
   },
 };
